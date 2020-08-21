8de338202b23484804de0fd0df57b253dbd70915
Tomas Poveda <tpovedatd@gmail.com>
2020-08-21 23:41:12 +0200

    Small fixes in versioneer setup

--- artellapipe/tools/namemanager/__version__.py
+++ artellapipe/tools/namemanager/__version__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Version module for artellapipe-tools-artellamanager
+Version module for artellapipe-tools-namemanager
 """
 
 from __future__ import print_function, division, absolute_import
